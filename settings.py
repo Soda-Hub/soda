c017c99990d06f2ec80aaf64cec7183d1c0c1cee
Victor Neo <icyisamu@gmail.com>
2020-05-02 16:42:16 +0800

    Fix requirements

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from starlette.config import Config
 config = Config('.env')
 DEBUG = config('DEBUG', cast=bool, default=False)
 DATABASE_URL = config('DATABASE_URL')
-DATABASE_TEST_URL = config('DATABASE_TEST_URL')
+DATABASE_TEST_URL = config('DATABASE_TEST_URL', default=None)
 TESTING = config('TESTING', cast=bool, default=False)
 
 
